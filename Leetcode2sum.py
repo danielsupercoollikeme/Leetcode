76a218bf9d3d378582122eb961b18dd8724530c3
danielsupercoollikeme <danielsupercoollikeme@gmail.com>
2024-01-23 09:22:26 +1300

    leetcode2sum myself

--- Leetcode2sum.py
+++ Leetcode2sum.py
@@ -14,3 +14,14 @@ class Solution(object):
                     continue
                 break
         return i, x
+    def solution_bruteforce(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if i != j and nums[i] + nums[j] == target:
+                    return[i, j]
+    def solution_dictionary(self, nums, target):
+        hashmap = {}
+        for i in range(len(nums)):
+            if target - nums[i] in hashmap:
+                return [hashmap[target - nums[i]], i]
+            hashmap[nums[i]] = i
